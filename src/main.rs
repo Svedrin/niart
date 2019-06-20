eda2ee5ea6d46217b9a0d16d41868e055296c8dd
Michael Ziegler <michael.ziegler@proemion.com>
2019-06-20 22:27:15 +0200

    the paint example does precisely what I want

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,78 @@
 extern crate piston_window;
+extern crate image as im;
+extern crate vecmath;
+
+// I need a
+// https://raw.githubusercontent.com/PistonDevelopers/piston-examples/master/src/paint.rs
+// is what I need!
 
 use piston_window::*;
-use piston::input::{RenderEvent, UpdateEvent, PressEvent, MouseCursorEvent};
-use piston::input::ButtonState;
+use vecmath::*;
 
 fn main() {
-    let width = 640;
-    let height  = 480;
-
+    let (width, height) = (640, 480);
     let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [width, height])
+        WindowSettings::new("piston: paint", (width, height))
         .exit_on_esc(true)
         .build()
         .unwrap();
 
+    let mut canvas = im::ImageBuffer::new(width, height);
+    let mut draw = false;
     let mut texture_context = window.create_texture_context();
+    let mut texture: G2dTexture = Texture::from_image(
+        &mut texture_context,
+        &canvas,
+        &TextureSettings::new()
+    ).unwrap();
 
-    let mut le_bouton_depressed = false;
-    let mut dem_rails = vec![false; (width * height) as usize];
+    let mut last_pos: Option<[f64; 2]> = None;
 
     while let Some(e) = window.next() {
-        //println!("Random Event! {:?}", e);
-        e.mouse_cursor(|pos| {
-            println!("Mouse cursor is at {}x{}", pos[0], pos[1]);
-            if le_bouton_depressed {
-                dem_rails[(pos[1] as usize * width as usize) + pos[0] as usize] = true;
-            }
+        if let Some(_) = e.render_args() {
+            texture.update(&mut texture_context, &canvas).unwrap();
+            window.draw_2d(&e, |c, g, device| {
+                // Update texture before rendering.
+                texture_context.encoder.flush(device);
 
-        });
-        e.button(|le_bouton| {
-            println!("Got a le bouton! {:?}", le_bouton);
-            le_bouton_depressed = match le_bouton.state {
-                ButtonState::Press   => true,
-                ButtonState::Release => false,
-            };
-        });
-        window.draw_2d(&e, |c, g, _device| {
-            clear([1.0; 4], g);
-            for x in 0..width {
-                for y in 0..height {
-                    if dem_rails[((y * width) + x) as usize] {
-                        ellipse_from_to(
-                            [0., 0., 0., 1.],
-                            [x as f64, y as f64],
-                            [(x+1) as f64, (y+1) as f64],
-                            c.transform,
-                            g
-                        );
-                    }
-                }
+                clear([1.0; 4], g);
+                image(&texture, c.transform, g);
+            });
+        }
+        if let Some(button) = e.press_args() {
+            if button == Button::Mouse(MouseButton::Left) {
+                draw = true;
             }
-        });
+        };
+        if let Some(button) = e.release_args() {
+            if button == Button::Mouse(MouseButton::Left) {
+                draw = false;
+                last_pos = None
+            }
+        };
+        if draw {
+            if let Some(pos) = e.mouse_cursor_args() {
+                let (x, y) = (pos[0] as f32, pos[1] as f32);
+
+                if let Some(p) = last_pos {
+                    let (last_x, last_y) = (p[0] as f32, p[1] as f32);
+                    let distance = vec2_len(vec2_sub(p, pos)) as u32;
+
+                    for i in 0..distance {
+                        let diff_x = x - last_x;
+                        let diff_y = y - last_y;
+                        let delta = i as f32 / distance as f32;
+                        let new_x = (last_x + (diff_x * delta)) as u32;
+                        let new_y = (last_y + (diff_y * delta)) as u32;
+                        if new_x < width && new_y < height {
+                            canvas.put_pixel(new_x, new_y, im::Rgba([0, 0, 0, 255]));
+                        };
+                    };
+                };
+
+                last_pos = Some(pos)
+            };
+
+        }
     }
 }
