a536d3700fcae71ca29face3ef43cb71381feb95
Michael Ziegler <michael.ziegler@proemion.com>
2019-06-20 23:03:13 +0200

    e -> evt

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,8 @@ fn main() {
 
     let mut last_pos: Option<[f64; 2]> = None;
 
-    while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g, device| {
+    while let Some(evt) = window.next() {
+        window.draw_2d(&evt, |c, g, device| {
             texture.update(&mut texture_context, &canvas).unwrap();
             // Update texture before rendering.
             texture_context.encoder.flush(device);
@@ -37,19 +37,19 @@ fn main() {
             clear([1.0; 4], g);
             image(&texture, c.transform, g);
         });
-        if let Some(button) = e.press_args() {
+        if let Some(button) = evt.press_args() {
             if button == Button::Mouse(MouseButton::Left) {
                 draw = true;
             }
         };
-        if let Some(button) = e.release_args() {
+        if let Some(button) = evt.release_args() {
             if button == Button::Mouse(MouseButton::Left) {
                 draw = false;
                 last_pos = None
             }
         };
         if draw {
-            if let Some(pos) = e.mouse_cursor_args() {
+            if let Some(pos) = evt.mouse_cursor_args() {
                 let (x, y) = (pos[0] as f32, pos[1] as f32);
 
                 if let Some(p) = last_pos {
